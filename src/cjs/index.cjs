e44b608a0460b80561e0c64265e0f697b9ac3a72
lelbois <164905463+linuxfandudeguy@users.noreply.github.com>
2025-01-21 13:28:47 -0500

    Create index.cjs

---
+++ src/cjs/index.cjs
@@ -0,0 +1,9 @@
+const transliterate = require('./mappings/transliterate.js');
+const toRussian = require('./mappings/toRussian.js');
+
+const Russo = {
+  transliterate,
+  toRussian
+};
+
+module.exports = Russo;
