40399867896523f3dbec5eafe804907f6f9b9ad1
lelbois <164905463+linuxfandudeguy@users.noreply.github.com>
2025-01-21 13:00:50 -0500

    Update index.umd.js

--- src/umd/index.umd.js
+++ src/umd/index.umd.js
@@ -7,16 +7,16 @@
     define(factory);
   } else if (typeof exports === 'object' && typeof module === 'object' && !module.exports) {
     // ESM (Node.js with ESM support)
-    import { transliterate } from './mappings/transliterate.js';
-    import { toRussian } from './mappings/toRussian.js';
+    import { transliterate } from './mappings/transliterate.umd.js';
+    import { toRussian } from './mappings/toRussian.umd.js';
     root.Russo = factory();
   } else {
     // Global (Browser)
     root.Russo = factory();
   }
 }(this, function () {
-  const transliterate = require('./mappings/transliterate.js');
-  const toRussian = require('./mappings/toRussian.js');
+  const transliterate = require('./mappings/transliterate.umd.js');
+  const toRussian = require('./mappings/toRussian.umd.js');
 
   return {
     transliterate,
