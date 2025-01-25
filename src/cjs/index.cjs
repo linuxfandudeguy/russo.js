677f2ec8360e2e02779e03a07e6b77d3cad3e65d
lelbois <164905463+linuxfandudeguy@users.noreply.github.com>
2025-01-25 18:26:43 -0500

    Delete src/cjs directory

--- src/cjs/index.cjs
+++
@@ -1,9 +0,0 @@
-const transliterate = require('./mappings/transliterate.cjs');
-const toRussian = require('./mappings/toRussian.cjs');
-
-const Russo = {
-  transliterate,
-  toRussian
-};
-
-module.exports = Russo;
