1ee8a2f5dfd3ff4995389dcbb6bf4c4a1635ad21
lelbois <164905463+linuxfandudeguy@users.noreply.github.com>
2025-01-26 17:42:25 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@
  * Licensed under the MIT License
  * GitHub: https://github.com/linuxfandudeguy/russo.js
  */
+
+// imports for the functions
 import { transliterate } from './mappings/transliterate.js';
 import { toRussian } from './mappings/toRussian.js';
-
+// attach to object
 const Russo = {
   transliterate,
   toRussian
 };
-
+// attach to object for browser
 if (typeof window !== 'undefined') {
   window.Russo = Russo;
 }
