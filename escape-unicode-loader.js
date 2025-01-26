c47415b1e42a04436bb9bb375f1ed20ee60610a2
lelbois <164905463+linuxfandudeguy@users.noreply.github.com>
2025-01-25 19:49:17 -0500

    Delete escape-unicode-loader.js

--- escape-unicode-loader.js
+++
@@ -1,7 +0,0 @@
-export default function(source) {
-  const escapedSource = source.replace(/[^\x00-\x7F]/g, (match) => {
-    return '\\u' + match.charCodeAt(0).toString(16).padStart(4, '0');
-  });
-  
-  return escapedSource;
-}
