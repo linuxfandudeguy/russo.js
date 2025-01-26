8271eebd44622c017448d1ef2201b79bcd3bff5a
lelbois <164905463+linuxfandudeguy@users.noreply.github.com>
2025-01-25 20:15:24 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import { transliterate } from './mappings/transliterate.js';
 import { toRussian } from './mappings/toRussian.js';
 
-export const Russo = {
+const Russo = {
   transliterate,
   toRussian
 };
+
+if (typeof window !== 'undefined') {
+  window.Russo = Russo;
+}
+
+export { Russo, transliterate, toRussian };
+export default Russo; 
